7e0f6204ecdca661728e090356e5a664e52a65a7
bumatcodingfr <charlieot23@gmail.com>
2026-02-11 20:35:36 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+if (distance <= 0) {
+  document.getElementById("countdown").innerHTML =
+    "<div class='finished'>FILES DECLASSIFIED — ACCESS GRANTED 🚨</div>";
+
+  document.getElementById("downloads").style.display = "block";
+  return;
+}
